1e2a479d322442923c7daea38fa138b7291c6a04
ryotokurata <t.d.o.a.its.m.l@i.softbank.jp>
2019-10-31 13:55:01 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
   devise_for :users
   root 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update, :index] do
   resources :messages, only: [:index, :create]
   end
->>>>>>> Stashed changes
 end
 
 
