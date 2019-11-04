66c90758a602299886c147d5b2c6ff1cb593477e
ryotokurata <t.d.o.a.its.m.l@i.softbank.jp>
2019-11-04 19:49:06 +0900

    自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,9 @@
 $(document).on('turbolinks:load', function(){
   function buildMessage(message){
-    var img = (message.image) ? `<img class= "chat__main__messages__message__lower-message__image" src=${message.image} >` : "";
-    var html = `<div class="chat__main__messages__message">
+    
+    var img = (message.image.url) ? `<img class= "chat__main__messages__message__lower-message__image" src=${message.image.url} >` : "";
+    if (message.content && img) {
+    var html = `<div class="chat__main__messages__message"data-id="${message.id}">
     <div class="chat__main__messages__message__upper-message">
       <div class="chat__main__messages__message__upper-message__user-name">
           ${message.name}
@@ -17,7 +19,43 @@ $(document).on('turbolinks:load', function(){
           ${img} 
     </div>
   </div>`
-     return html;
+    } else if (message.content) {
+     var html = `<div class="chat__main__messages__message"data-id="${message.id}">
+     <div class="chat__main__messages__message__upper-message">
+       <div class="chat__main__messages__message__upper-message__user-name">
+           ${message.name}
+       </div>
+       <div class="chat__main__messages__message__upper-message__date">
+           ${message.created_at}
+       </div>
+     </div>
+     <div class="chat__main__messages__message__lower-message">
+       <p class="chat__main__messages__message__lower-message__content">
+           ${message.content}
+       </p>
+           ${img} 
+     </div>
+   </div>`
+    } else if (img) {
+      var html = `<div class="chat__main__messages__message"data-id="${message.id}">
+     <div class="chat__main__messages__message__upper-message">
+       <div class="chat__main__messages__message__upper-message__user-name">
+           ${message.name}
+       </div>
+       <div class="chat__main__messages__message__upper-message__date">
+           ${message.created_at}
+       </div>
+     </div>
+     <div class="chat__main__messages__message__lower-message">
+       <p class="chat__main__messages__message__lower-message__content">
+           ${message.content}
+       </p>
+           ${img} 
+     </div>
+   </div>`
+   };
+   return html;
+     
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -33,6 +71,7 @@ $(document).on('turbolinks:load', function(){
     })
 
     .done(function(message){
+      console.log(message);
       var html = buildMessage(message);
       $('.chat__main__messages').append(html)
       $("#new_message")[0].reset();
@@ -44,7 +83,35 @@ $(document).on('turbolinks:load', function(){
       alert('投稿がありません');
     })
     .always(function(){
-      $('.chat__mchain__form__submit').prop('disabled', false);
+      $('.chat__main__form__submit').prop('disabled', false);
     })
+    
   })
+
+    function reloadMessages() {
+        var last_message_id = $('.chat__main__messages__message:last').data("message-id");
+        $.ajax({
+          //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+          url: 'api/messages',
+          //ルーティングで設定した通りhttpメソッドをgetに指定
+          type: 'get',
+          dataType: 'json',
+          //dataオプションでリクエストに値を含める
+          data: {last_id: last_message_id}
+        })
+        .done(function(messages) {
+          var insertMessage = '';
+          messages.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+            insertMessage = buildMessage(message); //メッセージが入ったHTMLを取得
+            $('.chat__main__messages').append(insertMessage);//メッセージを追加
+          })
+          $('.chat__main__messages').animate({scrollTop: $('.chat__main__messages')[0].scrollHeight}, 'fast');
+          
+        })
+        .fail(function() {
+          alert('自動更新に失敗しました');
+        });
+        
+    };
+  setInterval(reloadMessages,10000);
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,5 @@
+json.(@message, :content, :image)
 json.name @message.user.name 
-json.content  @message.content
 json.created_at @message.created_at.strftime("%Y/%m/%d %H:%M")
-json.image  @message.image.url
+json.id @message.id
 
